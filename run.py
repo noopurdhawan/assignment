b0dc40152db6fb05218f18d502dcad31b7d04678
Noopur Dhawan <noopurdhawan28@gmail.com>
2021-03-22 10:44:24 +0530

    Abort service if user_config setings not set.

--- run.py
+++ run.py
@@ -12,9 +12,14 @@ try:
     parser = configparser.ConfigParser()
     parser.read('user_config.ini')
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(parser['user_settings']['filename'])
-    logging.info('Configurations done!!')
-    # run flask app services
-    from app import app
+    if os.environ["GOOGLE_APPLICATION_CREDENTIALS"]:
+        logging.info('Configurations done with file:{} '.format(os.getenv('GOOGLE_APPLICATION_CREDENTIALS')))
+        # run flask app services
+        from app import app
+    else:
+        logging.info('Please check the config file settings.')
+        # abort services if config file is empty
+        os.abort()
 
 except Exception as e:
     logging.info(e)
