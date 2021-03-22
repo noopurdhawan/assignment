3fe13c60124fb3677670e88afc83afaba24252d0
Noopur Dhawan <noopurdhawan28@gmail.com>
2021-03-22 08:23:17 +0530

    Add comments for utils.py for s2sphere for level 16

--- app/utils.py
+++ app/utils.py
@@ -3,9 +3,10 @@ from s2 import s2
 
 def geotos2(latitude, longitude):
     """
-
-    :return:
+    Convert latitude and longitufe to s2sphere with level 16
+    :param latitude: latitiude of the location
+    :param longitude: longitude of the location
+    :return: s2sphere with level 16
     """
-
     level = 16
     return str(s2.geo_to_s2(latitude, longitude, level))
